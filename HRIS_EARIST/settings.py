39128e10b65df4794a42262b6d3cfd5fac7aa14a
DhaniSanJose <furrdoy@gmail.com>
2024-03-03 20:13:51 +0800

    Fifth Commit

--- HRIS_EARIST/settings.py
+++ HRIS_EARIST/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'images/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static")
+    os.path.join(BASE_DIR, "static")
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.home, name="home"),
-      path("products/", views.products, name='products'),
+        path('register/', views.registerPage, name='register'),
+        path('login/', views.loginPage, name='login'),
+        path('logout/', views.logoutUser, name='logout'),
+
+        path("", views.home, name="home"),
+        path('user/', views.userPage, name='user-page'),
+
+        path('account/', views.accountSettings, name="account"),
+
+        path("products/", views.products, name='products'),
         path("customer/<str:pk_test>/", views.customer, name='customer'),
         path('create_order/<str:pk>/', views.createOrder, name='create_order'),
         path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
